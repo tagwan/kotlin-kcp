ccfa826818c3df5c1811c9b6e1be077c14cf3d48
Espresso <monokai@qq.com>
2022-06-01 13:34:12 +0800

    style: code format

--- src/test/kotlin/TestUdp.kt
+++ src/test/kotlin/TestUdp.kt
@@ -4,22 +4,7 @@ import io.netty.channel.*
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.DatagramPacket
 import io.netty.channel.socket.nio.NioDatagramChannel
-import java.nio.charset.Charset
-import java.text.SimpleDateFormat
-import java.util.*
 import kotlin.test.Test
-
-
-class TestChannelInitializer : ChannelInitializer<NioDatagramChannel>() {
-
-    @Throws(Exception::class)
-    override fun initChannel(ch: NioDatagramChannel) {
-        val pipeline = ch.pipeline()
-        // 解码转String，注意调整自己的编码格式GBK、UTF-8
-        //pipeline.addLast("stringDecoder", new StringDecoder(Charset.forName("GBK")));
-        pipeline.addLast(TestServerHandler())
-    }
-}
 
 class TestServerHandler : SimpleChannelInboundHandler<DatagramPacket>() {
 
@@ -43,7 +28,7 @@ class TestServerHandler : SimpleChannelInboundHandler<DatagramPacket>() {
         //向客户端发送消息
         val json = "通知：我已经收到你的消息\r\n"
         // 由于数据报的数据是以字符数组传的形式存储的，所以传转数据
-        val bytes = json.toByteArray(Charset.forName("GBK"))
+        val bytes = json.toByteArray(Charsets.UTF_8)
         val data = DatagramPacket(Unpooled.copiedBuffer(bytes), packet.sender())
         ctx.writeAndFlush(data) //向客户端发送消息
     }
@@ -61,7 +46,14 @@ class TestUdp {
                 .option(ChannelOption.SO_BROADCAST, true) //广播
                 .option(ChannelOption.SO_RCVBUF, 2048 * 1024) // 设置UDP读缓冲区为2M
                 .option(ChannelOption.SO_SNDBUF, 1024 * 1024) // 设置UDP写缓冲区为1M
-                .handler(TestChannelInitializer())
+                .handler(object : ChannelInitializer<NioDatagramChannel>() {
+
+                    @Throws(Exception::class)
+                    override fun initChannel(ch: NioDatagramChannel) {
+                        val pipeline = ch.pipeline()
+                        pipeline.addLast(TestServerHandler())
+                    }
+                })
 
             val f: ChannelFuture = b.bind(7397).sync()
             f.channel().closeFuture().sync()
